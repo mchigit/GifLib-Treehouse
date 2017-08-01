af5d3c1afb413b786f7644eeb04c118845bc2a0f
michael <michael.chi1997@gmail.com>
2017-07-31 23:08:23 -0400

    Configured Spring with hibernate

--- src/main/java/com/teamtreehouse/giflib/config/DataConfig.java
+++ src/main/java/com/teamtreehouse/giflib/config/DataConfig.java
@@ -1,5 +1,6 @@
 package com.teamtreehouse.giflib.config;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 /**
  * Created by MICHAELChi on 2017-07-28.
@@ -27,7 +29,18 @@ public class DataConfig {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setConfigLocation(config);
         sessionFactory.setPackagesToScan(env.getProperty("giflib.entity.package"));
+        sessionFactory.setDataSource(dataSource());
         return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(env.getProperty("giflib.db.driver"));
+        ds.setUrl(env.getProperty("giflib.db.url"));
+        ds.setUsername(env.getProperty("giflib.db.username"));
+        ds.setPassword(env.getProperty("giflib.db.password"));
+        return ds;
     }
 
 }
